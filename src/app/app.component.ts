b635f0d2cec71c2ecf76143bfb4e7e7cc232c0a9
mabushiisign <mabushii.sign@gmail.com>
2023-11-12 22:07:09 +0900

    ESLint, Prettierの設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `
+    <div>Hello {{ value }}</div>
+  `,
 })
 export class AppComponent {
   value = 'World';
